8c76445ae4d7a4a5197b48d5ad43eb27eeb33bc5
nafanrasyid <52875994+nafanrasyid@users.noreply.github.com>
2025-02-19 23:34:42 +0700

    Add files via upload

---
+++ Nafan.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM:", nim)
+    print("Peran saya yaitu:", peran)
+
+if __name__ == "__main__":
+    nama = "Nafan Rasyidi hartono"
+    nim = "22.130.10.343"
+    peran = "Member"
+    perkenalan(nama, nim, peran)
